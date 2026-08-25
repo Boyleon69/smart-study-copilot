e332560eb566258f1725da883a5b91ff913a1726
Nicholas Leon <nsleon96@gmail.com>
2026-08-24 21:37:44 -0500

    added working navigation links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,15 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav className="bg-slate-900 text-white p-4">
       <div className="flex gap-6">
-        <span>Home</span>
-        <span>Dashboard</span>
-        <span>Upload</span>
-        <span>Flashcards</span>
-        <span>Quizzes</span>
-        <span>Settings</span>
+        <Link href="/">Home</Link>
+        <Link href="/dashboard">Dashboard</Link>
+        <Link href="/upload">Upload</Link>
+        <Link href="/flashcards">Flashcards</Link>
+        <Link href="/quizzes">Quizzes</Link>
+        <Link href="/settings">Settings</Link>
       </div>
     </nav>
   );
